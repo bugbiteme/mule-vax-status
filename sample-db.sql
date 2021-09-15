83890bd385f29a4d23d4c03ab1e1d7322b0df59f
bugbiteme <leon.levy@leonlev-ltmm1bz.internal.salesforce.com>
2021-09-15 15:11:02 -0700

    added additional data

--- sample-db.sql
+++ sample-db.sql
@@ -13,7 +13,7 @@ CREATE TABLE `person` (
 
 CREATE TABLE `dose` (
   `iddose` int NOT NULL AUTO_INCREMENT,
-  `type` varchar(45) COLLATE utf8mb4_la_0900_ai_ci DEFAULT NULL,
+  `maker` varchar(45) COLLATE utf8mb4_la_0900_ai_ci DEFAULT NULL,
   `doa` varchar(45) COLLATE utf8mb4_la_0900_ai_ci DEFAULT NULL,
   `provider` varchar(45) COLLATE utf8mb4_la_0900_ai_ci DEFAULT NULL,
   `lotNumber` varchar(45) COLLATE utf8mb4_la_0900_ai_ci DEFAULT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE `dose` (
 INSERT INTO person (idp, firstName, lastName, dob)
 VALUES (1, 'Jack', 'Sheppard', DATE '1992-05-18');
 
-INSERT INTO dose (type, doa, provider, lotNumber, fk_idp)
+INSERT INTO dose (maker, doa, provider, lotNumber, fk_idp)
 VALUES ( 
 		'Pfizer', 
         '3/22/21', 
@@ -36,7 +36,7 @@ VALUES (
         (SELECT idp FROM person WHERE firstName= 'Jack' AND lastName= 'Sheppard')
 );
 
-INSERT INTO dose (type, doa, provider, lotNumber, fk_idp)
+INSERT INTO dose (maker, doa, provider, lotNumber, fk_idp)
 VALUES ( 
 		'Pfizer', 
         '4/19/21', 
@@ -48,7 +48,7 @@ VALUES (
 INSERT INTO person (idp, firstName, lastName, dob)
 VALUES (2, 'Aden', 'Episcopio', DATE '1994-09-11');
 
-INSERT INTO dose (type, doa, provider, lotNumber, fk_idp)
+INSERT INTO dose (maker, doa, provider, lotNumber, fk_idp)
 VALUES ( 
 		'Johnson & Johnson', 
         '5/25/21', 
@@ -63,3 +63,14 @@ VALUES (3, 'Max', 'Mule', DATE '1991-04-06');
 SELECT * FROM person;
 
 SELECT * FROM dose;
+
+SELECT 
+    person.idp, person.firstName, person.lastName, person.dob, dose.maker, dose.doa, dose.provider, dose.lotNumber
+FROM 
+    person
+INNER JOIN 
+    dose 
+ON
+    person.idp=dose.fk_idp
+WHERE 
+    person.firstName='Jack' AND person.lastName='Sheppard' AND person.dob='1992-05-18';
